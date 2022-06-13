aa29a3cd2de8d113b616981a0ffd3a451823100a
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-13 17:08:22 +0200

    Add tests for ResultBox USD-PLN, change numbers formatting

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -7,11 +7,15 @@ import '@testing-library/jest-dom/extend-expect';
       render(<ResultBox from="PLN" to="USD" amount={100} />)
     });
 
+    it('should render without crashing, second case', () => {
+      render(<ResultBox from="USD" to="PLN" amount={100} />)
+    });
+
     const testCases = [
       { amount: '100'},
       { amount: '20,8'},
       { amount: '200.2'},
-      { amount: '345.50'},
+      { amount: '345558.50'},
     ];
     
     for(const testAmount of testCases) {
@@ -24,8 +28,25 @@ import '@testing-library/jest-dom/extend-expect';
           const resultBoxElement = screen.getByTestId('result-box');
   
           // check if element presents correct value
-          const result = (amount/3.5).toFixed(2);
-          expect(resultBoxElement).toHaveTextContent(`PLN ${amount.toFixed(2)} = $${result}`);
+          const result = (amount/3.5).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          const amountWithCommas = amount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          expect(resultBoxElement).toHaveTextContent(`PLN ${amountWithCommas} = $${result}`);
+      });
+    }
+
+    for(const testAmount of testCases) {
+      it('should render proper info about conversion when USD -> PLN', () => {
+        const amount = parseInt(testAmount.amount);
+        // render component
+        render(<ResultBox from="USD" to="PLN" amount={amount} />)
+  
+          // find “resultBox”
+          const resultBoxElement = screen.getByTestId('result-box');
+  
+          // check if element presents correct value
+          const result = (amount*3.5).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          const amountWithCommas = amount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          expect(resultBoxElement).toHaveTextContent(`$${amountWithCommas} = PLN ${result}`);
       });
     }
 });
